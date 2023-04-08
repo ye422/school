29476d11c45a57e747190ae70e1bb1b48648b8ff
ye422 <ye422@naver.com>
2023-04-08 14:06:36 +0900

    23.04.08 fix of coditional.c; enable to consider negative numbers

--- Class/CSE3030/Lab1/1-3/conditional.c
+++ Class/CSE3030/Lab1/1-3/conditional.c
@@ -1,5 +1,4 @@
 #include "conditional.h"
-
 /*
  * conditional(x, y, z): same as x ? y : z
  *   Example: conditional(2,4,5) = 4
@@ -8,11 +7,10 @@
 int conditional(int x, int y, int z) {
 
 	int result;
-	int t_f;
-	t_f = !x;
-	t_f += 0xFF;
+	int t_f = !x;
+	int mask = 0xFF << 8;
+	mask += 0xFF;
+	t_f += mask;
 	result = (t_f & y) | ( ~t_f & z );
-	
-
 	return result;
 }
